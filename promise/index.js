4f7a83c01f76688245bcf0f13cccbbea270d96af
caozhongjie <848773542@qq.com>
2020-08-03 08:18:46 +0800

    three

--- promise/index.js
+++ promise/index.js
@@ -4,83 +4,105 @@ class HD{
     static REJECTED = 'rejected'
     constructor(executor) {
         this.status = HD.PENDING // 状态
-        this.value = null
+        this.value = null // resolve返回的值
+        this.reason = null // reject返回的值
         this.callbacks = [] // 存放异步操作时之后要执行的函数
         try {
             executor(this.resolve.bind(this), this.reject.bind(this))  //只有改变this的指向,resolve中才能拿到status的值
         }catch (e) {
             this.reject(e)
         }
-
     }
     resolve(value) { // 这里的this是严格模式下的this指向，指向的是window,使用bind()绑定this
+        console.log(value)
+        console.log(this.status)
         if(this.status === HD.PENDING) { // 让状态不可更改，只能从pending->resolve
             this.status = HD.FULFILLED
             this.value = value
-            this.callbacks.map(callback => {
-                callback.onFulfilled(value)
-            })
-            // if(this.callbacks.length > 0) {
-            //     this.callbacks.map(callback => {
-            //         callback.onFulfilled(value)
-            //     })
-            // }
+            console.log(value)
+            if(this.callbacks.length > 0) { // 如果大于0，则表示为异步
+                setTimeout(() => { // 加上异步使在调用时保持resolve为异步执行
+                    this.callbacks.map(callback => {
+                        callback.onFulfilled(value)
+                    })
+                })
+            }
         }
     }
     reject(reason) {
         if(this.status === HD.PENDING) {
             this.status = HD.REJECTED
             this.value = reason
-            this.callbacks.map(callback => {
-                callback.onRejected(reason)
-            })
+            if(this.callbacks.length > 0) {
+                setTimeout(() => {
+                    this.callbacks.map(callback => {
+                        callback.onRejected(reason)
+                    })
+                })
+            }
         }
     }
-    then(onFulfilled, onRejected) {
+    then(onFulfilled, onRejected) { //为了进行链式操作,then()必须返回一个promise，只有promise才有then方法
         if(typeof onFulfilled !== "function") { // 如果传递进来的不是一个函数，那么就转成函数
-            onFulfilled = ()=>{}
+            onFulfilled = ()=>this.value // 当then()没有传递参数进来，或者参数不为函数时，此时直接调用value值传递给下一层then
         }
         if(typeof onRejected !== "function") {
-            onRejected = ()=>{}
+            onRejected = ()=>this.value
         }
-        if(this.status === HD.PENDING) { //如果resolve和reject还没有执行(异步操作)就调用了then方法，此时就将函数保存起来，在resolve或者reject执行后再进行调用
-            this.callbacks.push({
-                onFulfilled: value => { // onFuifilled对象属性
-                    try { // 捕获错误
-                        onFulfilled(value) // 形参中的函数，两者不同
-                    }catch (e) {
-                        onRejected(e)
+        return new HD((resolve, reject) => {
+            if(this.status === HD.PENDING) { //如果resolve和reject还没有执行(异步操作)就调用了then方法，此时就将函数保存起来，在resolve或者reject执行后再进行调用
+                this.callbacks.push({
+                    onFulfilled: value => { // onFuifilled对象属性
+                        try { // 捕获错误
+                            let result = onFulfilled(value) // 形参中的函数，两者不同
+                            resolve(result)
+                        }catch (e) {
+                            reject(e)
+                        }
+                    },
+                    onRejected: value => {
+                        try {
+                            let result =  onRejected(value)
+                            resolve(result)
+                        }catch (e) {
+                            reject(e)
+                        }
                     }
-                },
-                onRejected: value => {
+                })
+            }
+            if(this.status ===HD.FULFILLED) { //让其在对应的状态只能执行对应状态的函数
+                setTimeout(()=>{ // 为了实现promise的异步执行机制，then和catch中的回调是异步的
+                    try { // 将外部的错误捕获到里面来
+                      let result =  onFulfilled(this.value) // 在此时拿到then(resolve) return的值
+                        console.log(result)
+                        if(result instanceof HD) { // 如果返回的是一个promise对象
+                            result.then(resolve, reject)
+                            // result.then(value => {
+                            //     resolve(value)
+                            // }, reason => {
+                            //     reject(reason)
+                            // })
+                        } else {
+                            console.log(this.status)
+                            resolve(result)
+                        }
+
+                    }catch (e) {
+                        reject(e)
+                    }
+                },1000)
+            }
+            if(this.status === HD.REJECTED) {
+                setTimeout(()=>{ // 为了实现promise的异步执行机制，then和catch中的回调是异步的
                     try {
-                        onRejected(value)
+                      let result = onRejected(this.value)
+                       resolve(result)
                     }catch (e) {
-                        onRejected(e)
+                        reject(e)
                     }
-                }
-            })
-        }
-        if(this.status ===HD.FULFILLED) { //让其在对应的状态只能执行对应状态的函数
-            setTimeout(()=>{ // 为了实现promise的异步执行机制，then和catch中的回调是异步的
-                try { // 将外部的错误捕获到里面来
-                    onFulfilled(this.value)
-                }catch (e) {
-                    onRejected(e)
-                }
-            },1000)
-
-
-        }
-        if(this.status === HD.REJECTED) {
-            setTimeout(()=>{ // 为了实现promise的异步执行机制，then和catch中的回调是异步的
-                try {
-                    onRejected(this.value)
-                }catch (e) {
-                    onRejected(e)
-                }
-            })
-        }
+                })
+            }
+        })
     }
 }
 // new Promise()
